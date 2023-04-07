1924ac42aa6d5c1a8ad173044c3172076862dc66
Mark Washeim <blueprint@poetaster.de>
2023-04-07 14:12:06 +0200

    Fix: ip needs to be provided

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,7 +9,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Keypeer Api'}, pythonic_params=True)
-    app.run(port=8080)
+    app.run(host="2a01:4f9:3a:2414::12", port=8080)
 
 
 if __name__ == '__main__':
